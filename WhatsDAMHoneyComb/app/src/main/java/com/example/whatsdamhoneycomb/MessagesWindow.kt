36534958b230d7eac43465fa4d23dcf66b1e7ec3
isabel <isabelitavaliente@gmail.com>
2023-10-01 15:09:26 +0200

    Parte dos

--- WhatsDAMHoneyComb/app/src/main/java/com/example/whatsdamhoneycomb/MessagesWindow.kt
+++ WhatsDAMHoneyComb/app/src/main/java/com/example/whatsdamhoneycomb/MessagesWindow.kt
@@ -3,9 +3,19 @@ package com.example.whatsdamhoneycomb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import com.example.whatsdamhoneycomb.databinding.ActivityMessagesWindowBinding
+
+
 class MessagesWindow : AppCompatActivity() {
+    lateinit var binding: ActivityMessagesWindowBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_messages_window)
+        binding = ActivityMessagesWindowBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.sendMessage.setOnClickListener{
+            binding.messageText.text.clear()
+        }
     }
 }
+
