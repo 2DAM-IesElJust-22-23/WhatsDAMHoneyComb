32c8f1757aaad30a00f45c2427323f0815b3e5f0
Frank <fcarrerescatala@gmail.com>
2023-10-13 19:22:18 +0200

    RecycledView 1.0

---
+++ WhatsDAMHoneyComb/app/src/main/java/com/ieseljust/pmdm/MessageViewHolder.kt
@@ -0,0 +1,17 @@
+package com.ieseljust.pmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.content.Context
+import android.widget.CheckedTextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class MessageViewHolder(textView: View, val context:Context) : View.OnClickListener,RecyclerView.ViewHolder(textView) {
+
+    //val textM = itemView(msg_text)
+    override fun onClick(p0: View?) {
+    }
+
+}
---
+++ WhatsDAMHoneyComb/app/src/main/java/com/ieseljust/pmdm/Messages.kt
@@ -0,0 +1,9 @@
+package com.ieseljust.pmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.io.Serializable
+
+data class Messages(var nomUsuari:String, var mensaje:String):Serializable
+
+
